a2c61d62f14d19e1b62bff9980bbaa4722051ee6
jocelyn alsdorf & chase reckling <student@epicodus.com>
2015-04-20 11:22:49 -0700

    passed first if/else statement

--- lib/pingpong.rb
+++ lib/pingpong.rb
@@ -4,6 +4,8 @@ class Fixnum
     (1..self).to_a.each() do |item|
       if item.%(15) == 0
         myarray.push("pingpong")
+      else
+        myarray.push(item)
     end
   end
   myarray
--- spec/pingpong_spec.rb
+++ spec/pingpong_spec.rb
@@ -3,13 +3,16 @@ require('pingpong')
 
 describe('Fixnum#pingpong') do
   it("count form 1 to a given number") do
-  expect((5).pingpong()).to(eq([1,2,3,4,5]))
+  expect((2).pingpong()).to(eq([1,2]))
   end
   it("loop through each number") do
-  expect((5).pingpong()).to(eq([1,2,3,4,5]))
+  expect((2).pingpong()).to(eq([1,2]))
   end
-it("number is divisible by 15") do
-  expect((35).pingpong()).to(eq(["pingpong", "pingpong"]))
+  it("number is divisible by 15") do
+  expect((15).pingpong()).to(eq([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, "pingpong"]))
+  end
+  it("number is not divisible by 15 push item") do
+  expect((5).pingpong()).to(eq([1, 2, 3, 4, 5]))
 end
 
 end
